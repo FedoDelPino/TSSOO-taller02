46fc2ff256d79abf566445ca0b83eb384ea52a9f
FedoDelPino <fernando.delpino@alumnos.uv.cl>
2020-07-09 21:28:12 -0400

    Reduccion de lineas

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,5 @@
 #include <global.h>
 #include <checkArgs.hpp>
-#include <future>
-
-std::vector<uint32_t> ArrElementos;
 
 int main(int argc, char **argv)
 {
@@ -13,12 +10,12 @@ int main(int argc, char **argv)
 	int LSup;
 
 	uint32_t sumaSerial = 0;
-	//uint32_t sumaParcial = 0;
 	uint32_t sumaThreads = 0;
 	std::vector<std::thread *> threads;
 	std::vector<uint32_t> ArrElementos;
 	std::vector<uint32_t> SumasParciales;
 
+	//Entrada de Argumentos
 	auto argumentos = (std::shared_ptr<checkArgs>)new checkArgs(argc, argv);
 
 	totalElementos = argumentos->getArgs().tamProblema;
@@ -26,37 +23,23 @@ int main(int argc, char **argv)
 	lInf = argumentos->getArgs().lInf;
 	LSup = argumentos->getArgs().LSup;
 
+	//Generador de Numeros Aleatorios en cuanto a los Limites
 	static thread_local std::random_device device;
 	static thread_local std::mt19937 rng(device());
 	std::uniform_int_distribution<uint32_t> nRandom(lInf, LSup);
 
-	//	std::cout << "Elementos: " << totalElementos << std::endl;
-	//	std::cout << "Threads  : " << numThreads << std::endl;
-	//	std::cout << "Limite Inferior  : " << lInf << std::endl;
-	//	std::cout << "Limite Superior  : " << LSup << std::endl;
+	if (totalElementos < numThreads)
+	{
+		std::cout << "Por favor, corroborar que el tamaño del arreglo sea igual o mayor que los Hilos creados para realizar la correcta suma" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	//Posible Solucion: Agregar la condicional dentro del proceso de suma, para luego ajustar los rangos de corte y pasar un elemento a cada hilo, arreglar mas adelante
+	//Analizar, creo que lo tome bien, paso innecesario
 	for (size_t i = 0; i < totalElementos; i++)
 	{
 		ArrElementos.push_back(nRandom(rng));
 		SumasParciales.push_back(0);
 	}
-
-	//auto LlenadoArreglo = [](uint32_t totalElementos, std::vector<uint32_t> ArrElementos) {
-	//	for (size_t i = 0; i < totalElementos; i++)
-	//	{
-	//		ArrElementos.push_back(i);
-	//	}
-	//	return (ArrElementos);
-	//};
-
-	//for (size_t i = 0; i < numThreads; ++i)
-	//{
-	//	threads.push_back(new std::thread(LlenadoArreglo, std::ref(totalElementos), std::ref(ArrElementos)));
-	//}
-
-	//for (auto &th : threads)
-	//{
-	//	th->join();
-	//}
 
 	//======SERIAL======
 	for (auto &num : ArrElementos)
@@ -64,32 +47,16 @@ int main(int argc, char **argv)
 		sumaSerial += num;
 	}
 
-	auto sumaParcial = [](std::vector<uint32_t> &ArrElementos, std::vector<uint32_t> &SumasParciales, size_t pos, size_t Left, size_t Right) {
+	auto sumaParcial = [](std::vector<uint32_t> &ArrElementos, std::vector<uint32_t> &SumasParciales, size_t posArr, size_t Left, size_t Right) {
 		for (size_t i = Left; i < Right; ++i)
 		{
-			SumasParciales[pos] += ArrElementos[i];
+			SumasParciales[posArr] += ArrElementos[i];
 		}
-		return SumasParciales;
 	};
-
-	//======THREADS======
-	//(1) Separación del trabajo
-	//for (size_t i = 0; i < numThreads; i++)
-	//{
-	//	auto th = std::async(std::launch::async, sumaParcial, std::ref(ArrElementos), 0, ArrElementos.size() / 2);
-	//}
-	//auto t1 = std::async(std::launch::async, sumaParcial, std::ref(ArrElementos), 0, ArrElementos.size() / 2);
-	//auto t2 = std::async(std::launch::async, sumaParcial, std::ref(ArrElementos), ArrElementos.size() / 2, ArrElementos.size());
-
-	//sumaParcial1 = t1.get();
-	//sumaParcial2 = t2.get();
 
 	for (size_t i = 0; i < numThreads; ++i)
 	{
-		threads.push_back(new std::thread(sumaParcial,
-										  std::ref(ArrElementos), std::ref(SumasParciales), i,
-										  i * (totalElementos) / numThreads,
-										  (i + 1) * (totalElementos) / numThreads));
+		threads.push_back(new std::thread(sumaParcial, std::ref(ArrElementos), std::ref(SumasParciales), i, i * (totalElementos) / numThreads, (i + 1) * (totalElementos) / numThreads));
 	}
 	for (auto &th : threads)
 	{
@@ -98,7 +65,6 @@ int main(int argc, char **argv)
 	//(2) Reducción (Consolidación de resultados parciales)
 	for (auto &sumaTh : SumasParciales)
 	{
-		std::cout << "Suma de Parciales: " << sumaTh << std::endl;
 		sumaThreads += sumaTh;
 	}
 
